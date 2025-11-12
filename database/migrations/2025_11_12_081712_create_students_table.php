5cdc0c73ed1d6d5dde12ec74d442378e89340959
BenJambalos <wazooruckus@gmail.com>
2025-11-12 16:20:35 +0800

    new

--- database/migrations/2025_11_12_081712_create_students_table.php
+++ database/migrations/2025_11_12_081712_create_students_table.php
@@ -9,14 +9,10 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('items', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('course');
-            $table->string('email');
-            $table->string('number');
             $table->timestamps();
         });
     }
@@ -26,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('items');
+        Schema::dropIfExists('students');
     }
 };
